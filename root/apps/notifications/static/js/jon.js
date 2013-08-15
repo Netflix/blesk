50c63a6d1c153b6115a421f0263b23f7b89f41bf
Joe Sondow <jsondow@netflix.com>
2013-08-15 14:22:49 -0700

    Refactored
    Formatting, standardized whitespace, added missing semicolons, removed warnings
    Removed unnecessary code
    Removed unused argument
    Declared local var instead of accidental global var
    Renamed legacy function names
    Update storage key prefix
    Whitespace and comments
    Shorter camel case var names. Removed warning.

--- root/apps/notifications/static/js/jon.js
+++ root/apps/notifications/static/js/jon.js
@@ -1,203 +1,138 @@
-
-
 // wrapping everything into one function - so that nothing is polluting the rest of js code
-document.addEventListener('DOMContentLoaded',function(){
-    (function ()
-    {
+document.addEventListener('DOMContentLoaded', function () {
     // two functions
     // wrap() main functions that wraps
-    // setCookie - getCookier - functions to store and retrieve nots text (using localStorage instead of cookies)
+    // setValue - getValue - functions to store and retrieve notifications text (using localStorage instead of cookies)
     // showNotifications() process the notifcations and place it on the page
     // loadData() load the current notifications
     // addLoadEvent() - programming onload function
 
-
-
-    function setCookie(c_name,value,exdays)
-    {
-       if(typeof(Storage)!=="undefined")
-       {
-         Storage.prototype.setObject = function(key, lvalue) {
-             this.setItem(key, lvalue);
-         }
-         localStorage.setObject(c_name,value)
-     }
-     else
-     {
-        return null;
+    function setValue(key, value) {
+        if (window.localStorage) {
+            localStorage.setItem(key, value)
+        }
     }
-}
 
-function getCookie(c_name)
-{
-  if(typeof(Storage)!=="undefined")
-  {
-    Storage.prototype.getObject = function(key) {
-        var value = this.getItem(key);
-        return value;
+    function getValue(key) {
+        if (window.localStorage) {
+            return localStorage.getItem(key)
+        }
+        else {
+            return null;
+        }
     }
-    return localStorage.getObject(c_name)
-}
-else
-{
-    console.log("undefined")
-    return null;
-}
-return null;
-}
 
+    // defining global variable
+    var appId = "";
+    var targetElement = document.body;
+    if (document.getElementById("nf-notifier-div") != null) {
+        targetElement = document.getElementById("nf-notifier-div");
+        appId = targetElement.getAttribute("data-appid");
+        if (appId == null) {
+            appId = "default";
+        }
+    }
 
-        // defining global variable
-        var nf_notification_app_id = "";
-        var nf_notification_element_obj = document.body;
-        if (document.getElementById("nf-notifier-div") != null)
-        {
-            nf_notification_element_obj = document.getElementById("nf-notifier-div");
-            nf_notification_app_id = nf_notification_element_obj.getAttribute("data-appid");
-            if (nf_notification_app_id == null)
-            {
-                nf_notification_app_id = "default";
-            }        
+    // adding a wrapper for notification element
+    if (targetElement == document.body) {
+        //console.log("here");
+        var firstAfterBody = document.createElement("div");
+        document.body.insertBefore(firstAfterBody, document.body.firstChild);
+        targetElement = firstAfterBody;
+    }
+
+    var targetElementInnerHtml = targetElement.innerHTML;
+
+    // Create show notification data handler
+    var showNotification = function (data) {
+
+        var obj;
+        var notText = "";
+        var notType = "";
+
+        // storing notification overrider
+        // keyword = appId = all
+        var globalNotText = "";
+        var globalNotType = "";
+
+        //console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            //console.log(data[i][1].appId);
+            if (data[i][1].appId == appId) {
+                notText = data[i][1].message + " ";
+                notType = data[i][1].alertType;
+            }
+
+            if (data[i][1].appId == "all") {
+                globalNotText = data[i][1].message + " ";
+                globalNotType = data[i][1].alertType;
+            }
         }
 
-
-        // adding a wrapper for notification element
-        if (nf_notification_element_obj == document.body)
-        {
-            //console.log("here");
-            var firstAfterBody = document.createElement("div");
-            document.body.insertBefore(firstAfterBody, document.body.firstChild);
-            nf_notification_element_obj = firstAfterBody;
+        if (globalNotText) {
+            if (notText) {
+                notText = globalNotText + " | <strong>" + notType + ":</strong> " + notText;
+                notType = globalNotType;
+            }
+            else {
+                notText = globalNotText;
+                notType = globalNotType;
+            }
         }
 
-        var nf_notification_element_obj_html = nf_notification_element_obj.innerHTML;
+        if (notText.length > 0) {
+            var notificationText = document.createElement("div");
+            notificationText.id = "notification-div";
+            var styleForNotificationType = "";
+            notificationText.innerHTML = "<div style=\"float: left; " + styleForNotificationType + "\"><strong>" + notType + ": </strong>" + notText + "</div>";
+            notificationText.className = "nf-notifier";
+            var close = document.createElement("div");
+            close.className = "nf-notifier-close";
+            close.innerHTML = "&times;";
+            close.id = "nf-notifier-close-element";
+            notificationText.appendChild(close);
+            if (notText.length > 0) {
+                //alert(getValue("blesknotifications"));
+                if ((getValue("blesknotifications" + appId) == null) || (notText != getValue("blesknotifications" + appId))) {
 
-        // Create show notification data handler
-        var showNotification = function(data) {
+                    targetElement.innerHTML = "<div id=\"nf-notifier-element\" class=\"nf-notifier\">" + notificationText.innerHTML + "</div><div style=\"clear:both\"></div>" + targetElementInnerHtml;
+                    document.getElementById("nf-notifier-close-element").addEventListener("click", function () {
+                        document.getElementById("nf-notifier-element").style.display = "none";
+                        setValue("blesknotifications" + appId, notText);
+                    });
 
-            var notText = "";
-            var notType = "";
-
-            // storing notification overrider
-            // keyword = appId = all
-            var globalNotText = "";
-            var globalNotType = "";
-
-
-            //console.log(data);
-            for (var i = 0; i< data.length; i++)
-            {
-                //console.log(data[i][1].appId);
-                if (data[i][1].appId == nf_notification_app_id)
-                {
-                    notText = data[i][1].message + " ";
-                    notType = data[i][1].alertType;
                 }
-
-                if (data[i][1].appId == "all")
-                {
-                    globalNotText = data[i][1].message + " ";
-                    globalNotType = data[i][1].alertType;                    
+                else {
+                    // closing float
+                    targetElement.innerHTML = "<div style=\"clear:both\"></div>" + targetElementInnerHtml;
                 }
             }
-
-            if (globalNotText)
-            {
-                if (notText)
-                {
-                    notText = globalNotText + " | <strong>" +notType+":</strong> " + notText;
-                    notType = globalNotType;
-                }
-                else
-                {
-                    notText = globalNotText;
-                    notType = globalNotType;
-                }
+            else {
+                // closing float
+                targetElement.innerHTML = "<div style=\"clear:both\"></div>" + targetElementInnerHtml;
             }
 
-
-            if (notText.length > 0)
-            {
-                var notificationText = document.createElement("div");
-                notificationText.id = "notification-div";
-                var styleForNotificationType = "";
-                notificationText.innerHTML = "<div style=\"float: left; "+styleForNotificationType+"\"><strong>"+notType+": </strong>"+notText+"</div>";
-                notificationText.className = "nf-notifier";
-                var close = document.createElement("div");
-                close.className = "nf-notifier-close";
-                close.innerHTML = "&times;";
-                close.id = "nf-notifier-close-element";
-                notificationText.appendChild(close);
-                if (notText.length > 0)
-                {
-                    //alert(getCookie("notificationscookie"));
-                    if ((getCookie("notificationscookie"+nf_notification_app_id) == null)||(notText != getCookie("notificationscookie"+nf_notification_app_id)))
-                    {
-
-                        nf_notification_element_obj.innerHTML = "<div id=\"nf-notifier-element\" class=\"nf-notifier\">"+notificationText.innerHTML+"</div><div style=\"clear:both\"></div>"+nf_notification_element_obj_html;
-                        document.getElementById("nf-notifier-close-element").addEventListener("click", function() {
-                            document.getElementById("nf-notifier-element").style.display = "none";
-                            setCookie("notificationscookie"+nf_notification_app_id,notText,7);
-                        });
-                        
-                    }
-                    else
-                    {
-                        // closing float
-                        nf_notification_element_obj.innerHTML = "<div style=\"clear:both\"></div>"+nf_notification_element_obj_html;
-                    }
-                }
-                else
-                {
-                   // closing float
-                   nf_notification_element_obj.innerHTML = "<div style=\"clear:both\"></div>"+nf_notification_element_obj_html;
-               }
-
-           }
-           else
-           {
-                //console.log("removing");
-                obj = document.getElementById("nf-notifier-element");
-                if (obj != null)
-                {
-                    nf_notification_element_obj.removeChild(obj);
-                }
-
+        }
+        else {
+            //console.log("removing");
+            obj = document.getElementById("nf-notifier-element");
+            if (obj != null) {
+                targetElement.removeChild(obj);
             }
-            setTimeout(function(){loadData()},10000);
+
+        }
+        setTimeout(loadData, 10000);
+    };
+
+    // Regularly pulling for data
+    var loadData = function () {
+        var req = new XMLHttpRequest();
+        // TODO: Add server name to the set of div inputs
+        req.open("GET", "http://notifications.test.netflix.net/getAllNotificationsCached?time=" + Math.random(), true);
+        req.onload = function () {
+            showNotification(JSON.parse(req.responseText));
         };
-
-
-
-        // Regularly pulling for data
-        var loadData = function(){
-            var req = new XMLHttpRequest();
-            req.open("GET", "http://notifications.test.netflix.net/getAllNotificationsCached?time="+Math.random(), true);
-            req.onload = function() {
-                showNotification(JSON.parse(req.responseText));
-            };
-            req.send();
-
-        }
-
-
-    // function to invoke notification js on onload
-    // also making sure no current onload functions are overriden 
-    function addLoadEvent(func) {
-        var oldonload = window.onload;
-        if (typeof window.onload != 'function') {
-          window.onload = func;
-      } else {
-          window.onload = function() {
-            if (oldonload) {
-              oldonload();
-          }
-          func();
-      }
-  }
-}
-
-addLoadEvent(loadData);
-
-})();
+        req.send();
+    };
+    loadData();
 });
